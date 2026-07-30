9a49a700eafcde1b2656156ce56d7def2a055744
priyal-codes <priyaldotcodes@gmail.com>
2026-07-30 23:24:12 +0530

    fixed res.locals.currUser is null safe and wishlist chechecks validate array types

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     if (req.session) {
